33f11f9f85961c34679f9d0b45fdd77cdfb47703
Abhijnan Vegi <abhijnan.vegi@gmail.com>
2023-10-15 18:54:43 +0530

    Fixed comparator

--- src/comparator.cpp
+++ src/comparator.cpp
@@ -2,8 +2,9 @@
 
 bool RowCmp::operator()(const vector<int> &row1, const vector<int> &row2) {
     for (int i = 0; i < columnIndices.size(); i++) {
-        if (row1[columnIndices[i]] < row2[columnIndices[i]] and sortingStrategy[i] == ASC) return true;
-        if (row1[columnIndices[i]] > row2[columnIndices[i]] and sortingStrategy[i] == DESC) return true;
+        if ((row1[columnIndices[i]] == row2[columnIndices[i]]))  continue;
+        if (sortingStrategy[i] == DESC) return row1[columnIndices[i]] > row2[columnIndices[i]];
+        if (sortingStrategy[i] == ASC) return row1[columnIndices[i]] < row2[columnIndices[i]];
     }
     return false;
 }
